aee142d18ec47b1554815d75cb61a5b319859d48
Ahmed elbaz <ahmedelbaz462@gmail.com>
2026-04-18 13:09:06 +0200

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "iti-zag-394711685791"
-    key          = "iti/dev/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
